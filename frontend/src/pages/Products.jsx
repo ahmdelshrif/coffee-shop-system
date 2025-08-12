49df1fe2c9e1ffcf06c194de8f3aa89db2b87149
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2025-08-12 15:00:13 +0300

    Update Products.jsx

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { ThreeDot } from 'react-loading-indicators';
 import Swal from 'sweetalert2'
-
+import { FaCartPlus } from "react-icons/fa";
 const Products = () => {
   const [categoryOrder, setCategoryOrder] = useState([]);
   const [productsByCategory, setProductsByCategory] = useState({});
@@ -307,14 +307,15 @@ Swal.fire({
       [product._id]: value,
     }));
   }}
-  className="border px-2 py-1 rounded  mb-2 text-center w-[80px]"
+  className="border px-2 py-1 rounded mb-2 text-center w-[80px]"
 />
 
 <button
   onClick={() => addToOrder(product, quantities[product._id] || 1)}
-  className="bg-blue-500 text-white px-2 py-1 rounded text-sm hover:bg-blue-600"
+  className="bg-blue-500 text-white px-3 py-2 rounded text-sm hover:bg-blue-600 flex items-center justify-center"
+  title="إضافة للأوردر"
 >
-  إضافة للأوردر
+  <FaCartPlus size={18} />
 </button>
           </div>
           </div>
