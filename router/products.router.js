a09137336c88c02c103dad23bc0e1fb3eb80df52
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2025-08-13 16:13:13 +0300

    Update products.router.js

--- router/products.router.js
+++ router/products.router.js
@@ -1,27 +1,40 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {creatProduct,getProducts 
-      ,uploadproductsImages,resizeImage,
-      getSpecifiedProduct,updateProduct,deleteProduct
-}=require("../controller/products.controll")
+const {
+      creatProduct,
+      getProducts,
+      uploadproductsImages,
+      resizeImage,
+      getSpecifiedProduct,
+      // updateProduct,
+      deleteProduct,
+} = require("../controller/products.controll");
 
-const {protect,allowTO}=require("../controller/auth.coontroll")
-const {creatproduct_catchError,updateproduct_catchError
-      ,Specifiedproduct_catchError
-      ,deleteproduct_catchError}
-      =require("../utils/valdetor/productsvalidetor")
+const { protect, allowTO } = require("../controller/auth.coontroll");
+const {
+      creatproduct_catchError,
+      // updateproduct_catchError,
+      Specifiedproduct_catchError,
+      deleteproduct_catchError,
+} = require("../utils/valdetor/productsvalidetor");
 
-router.route("/").post(protect,allowTO("manager","admin")
- ,uploadproductsImages,resizeImage,creatproduct_catchError
- ,creatProduct)
-.get(protect,getProducts)
+router
+      .route("/")
+      .post(
+            protect,
+            allowTO("manager", "admin"),
+            uploadproductsImages,
+            resizeImage,
+            creatproduct_catchError,
+            creatProduct
+      )
+      .get(protect, getProducts);
 
-router.route("/:id").get(Specifiedproduct_catchError,getSpecifiedProduct)
+router
+      .route("/:id")
+      .get(Specifiedproduct_catchError, getSpecifiedProduct)
+      // .put(protect, allowTO("manager","admin"), uploadproductsImages, resizeImage, updateproduct_catchError, updateProduct)
+      .delete(protect, allowTO("manager", "admin"), deleteproduct_catchError, deleteProduct);
 
-// .put(protect,allowTO("manager","admin"),uploadproductsImages
-// ,resizeImage,updateproduct_catchError,updateProduct)
-
-.delete(protect,allowTO("manager","admin"),deleteproduct_catchError,deleteProduct)
-
-module.exports=router
+module.exports = router;
