6cbd8b7dda2dd1458627d523e4da4a4b1476a32c
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2025-08-13 16:12:38 +0300

    Update Erorr.js

--- middelweres/Erorr.js
+++ middelweres/Erorr.js
@@ -1,54 +1,42 @@
-const ApiError=require("../utils/apiError")
+const ApiError = require("../utils/apiError");
 
+const errorDetection = (err) => {
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new ApiError(message, 400);
+  }
+  return err;
+};
 
-errorDetection = (err)=>{
- if (err.name === 'CastError') {
-  const message = `Invalid ${err.path}: ${err.value}`;
-  return  err= new ApiError(message, 400);
-    }
-      return err
-}
-    
-exports.globalEorrs= (err,req,res,next)=>{
- err.statusCode = err.statusCode || 500;
- err.status = err.status || 'error';
- err=errorDetection(err);
-       if(process.env.NODE_ENV=="Devlopment"){
-        senderrorforev(err,res)
-       }else{
-        senderrorforproducct(err,res)
-       }
-       
-    }
+exports.globalEorrs = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  err = errorDetection(err);
 
-// عرض ال erorr في ال Devlopment
-const senderrorforev=(err,res)=>{
+  if (process.env.NODE_ENV === "development") {
+    sendErrorForDev(err, res);
+  } else {
+    sendErrorForProd(err, res);
+  }
+};
+
+const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     error: err,
     stack: err.stack,
   });
- }
+};
 
-
-// عرض ال erorr في ال producct
-const senderrorforproducct=(err,res)=>{
-     if(err.name==="JsonWebTokenError")
-        {       
-           err= new ApiError("invalid token ", 400);
-        }
-         else if(err.name==="TokenExpiredError"){
-           err= new ApiError("expirt token ", 400);
-          }
-          res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message,
-          }); 
-}
-
-
-
-  
-
-  
+const sendErrorForProd = (err, res) => {
+  if (err.name === "JsonWebTokenError") {
+    err = new ApiError("Invalid token", 400);
+  } else if (err.name === "TokenExpiredError") {
+    err = new ApiError("Expired token", 400);
+  }
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
